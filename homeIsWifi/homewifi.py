a07c154676629c95d368a4e364da1fa78f9ea667
Cyanide Cupcake <jlweichler@gmail.com>
2016-07-16 13:30:48 +1200

    home is wifi code from capital-e RPi

---
+++ homeIsWifi/homewifi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings (False)
+
+pins =[11,13,15,18]
+for i in pins:
+    GPIO.setup(i,GPIO.OUT)
+
+def switchLighting(x):
+    for i in pins:
+        print(i,x==i)
+##        if x==i :
+##            GPIO.output(i,GPIO.HIGH)
+##        else :
+##            GPIO.output(i,GPIO.OUT)
+        GPIO.output(i,x==i)
+
+while True:
+    for i in pins:
+        switchLighting(i)
+        time.sleep(15)
